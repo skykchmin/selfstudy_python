e893db39866f5a8ef18ca080fb1d20a2d169e98a
skykchmin <skykchmin@naver.com>
2020-04-07 23:35:47 +0900

    class 계산기 구현
    class 계산기 구현
    
    [200407]

---
+++ FourCal.py
@@ -0,0 +1,23 @@
+class Fourcal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
+    def sum(self):
+        result = self.first + self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result    
+
+a = Fourcal()
+b = Fourcal()
+a.setdata(4,2)
+b.setdata(3,7)
+a.sum()
